1ac0243d7ec02c16319a6d6625c89b3626244f44
Devansh <devansh.singh@resourcifi.com>
2022-11-10 12:03:19 +0530

    Stage1

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -656,3 +656,27 @@ if ( ! function_exists( 'wp_get_list_item_separator' ) ) :
 endif;
 
 
+function webp_upload_mimes( $existing_mimes ) {
+ // add webp to the list of mime types
+ $existing_mimes['webp'] = 'image/webp';
+ // return the array back to the function with our added mime type
+ return $existing_mimes;
+ }
+ add_filter( 'mime_types', 'webp_upload_mimes' );
+ //** * Enable preview / thumbnail for webp image files.*/
+ function webp_is_displayable($result, $path) {
+ if ($result === false) {
+ $displayable_image_types = array( IMAGETYPE_WEBP );
+ $info = @getimagesize( $path );
+ if (empty($info)) {
+ $result = false;
+ } elseif (!in_array($info[2], $displayable_image_types)) {
+ $result = false;
+ } else {
+ $result = true;
+ }
+ }
+ return $result;
+ }
+ add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
+
